3de707503ed8d7a351f142e25a7b79a8dabe3363
eylulbsrr <eylulbaser5784@gmail.com>
2026-01-22 01:48:42 +0300

    Main: Kullanıcı arayüzüne manuel tarih girişi ve Scanner kontrolü eklendi

--- src/kutuphane/Main.java
+++ src/kutuphane/Main.java
@@ -1,6 +1,7 @@
 package kutuphane;
 
 import java.util.Scanner;
+import java.time.LocalDate; // <--- YENİ: Tarih kütüphanesi eklendi
 
 public class Main {
 
@@ -39,13 +40,30 @@ public class Main {
                     System.out.print("Aranacak kelime: ");
                     library.searchLibrary(scanner.nextLine());
                     break;
+                    
+                // --- BURASI DEĞİŞTİ ---
                 case "2": 
                     System.out.print("Üye ID: ");
                     String mId = scanner.nextLine();
                     System.out.print("Kitap ISBN: ");
                     String isbn = scanner.nextLine();
-                    library.borrowBook(mId, isbn);
+                    
+                    // Kullanıcıdan tarihi string olarak alıyoruz
+                    System.out.print("Veriliş Tarihi (Yıl-Ay-Gün örn: 2025-12-20): ");
+                    String tarihGirdisi = scanner.nextLine();
+                    
+                    try {
+                        // String'i LocalDate'e çeviriyoruz
+                        LocalDate girilenTarih = LocalDate.parse(tarihGirdisi);
+                        
+                        // Metoda tarihi de gönderiyoruz
+                        library.borrowBook(mId, isbn, girilenTarih); 
+                    } catch (Exception e) {
+                        System.out.println("HATA: Tarih formatı yanlış! (Örnek: 2025-12-20 olmalı)");
+                    }
                     break;
+                // ----------------------
+
                 case "3": 
                     System.out.print("İade edilen Kitap ISBN: ");
                     library.returnBook(scanner.nextLine());
